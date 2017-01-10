d053e30d2dba5f71f2b7a5c7bda435b7f875d129
admin <liang826@hotmail.com>
2017-01-10 15:37:44 +0800

    add bootstrap modal and auto scrolling animation, sharing button link

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,79 +1,15 @@
 
-$(document).ready(function(){
-
-/**
- * This object controls the nav bar. Implement the add and remove
- * action over the elements of the nav bar that we want to change.
- *
- * @type {{flagAdd: boolean, elements: string[], add: Function, remove: Function}}
- */
-var myNavBar = {
-
-    flagAdd: true,
-
-    elements: [],
-
-    init: function (elements) {
-        this.elements = elements;
-    },
-
-    add : function() {
-        if(this.flagAdd) {
-            for(var i=0; i < this.elements.length; i++) {
-                document.getElementById(this.elements[i]).className += " fixed-theme";
-            }
-            this.flagAdd = false;
-        }
-    },
-
-    remove: function() {
-        for(var i=0; i < this.elements.length; i++) {
-            document.getElementById(this.elements[i]).className =
-                    document.getElementById(this.elements[i]).className.replace( /(?:^|\s)fixed-theme(?!\S)/g , '' );
-        }
-        this.flagAdd = true;
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
     }
-
-};
-
-/**
- * Init the object. Pass the object the array of elements
- * that we want to change when the scroll goes down
- */
-myNavBar.init(  [
-    "header",
-    "header-container",
-    "brand"
-]);
-
-/**
- * Function that manage the direction
- * of the scroll
- */
-function offSetManager(){
-
-    var yOffset = 0;
-    var currYOffSet = window.pageYOffset;
-
-    if(yOffset < currYOffSet) {
-        myNavBar.add();
-    }
-    else if(currYOffSet == yOffset){
-        myNavBar.remove();
-    }
-
-}
-
-/**
- * bind to the document scroll detection
- */
-window.onscroll = function(e) {
-    offSetManager();
-}
-
-/**
- * We have to do a first detectation of offset because the page
- * could be load with scroll down set.
- */
-offSetManager();
+  });
 });
